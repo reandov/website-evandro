5b06d92b8f12b929a03f3a974cb487544228b282
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2023-05-16 15:12:21 -0300

    🎨 improving timeline component

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import React from 'react'
 import { SectionTitle } from '../SectionTitle'
+import { Experience, Project } from '@/types/types'
 
 export function Timeline() {
   const experiences = [
@@ -10,16 +13,98 @@ export function Timeline() {
       role: 'Internship in Web/Mobile Development',
       description:
         'Worked on creating websites and mobile apps using React and React Native.',
+      projects: [
+        {
+          id: 0,
+          title: 'React Native app for hydromethers',
+          description:
+            'In this first project, I have built an app with a friend for hydromether registration for a small city here in Brazil. By the end of the project, the app was being used for dozens of employees and we have successfully gathered tons of data.',
+        },
+        {
+          id: 1,
+          title: 'React website for RFCs',
+          description:
+            'In the second project, I have built with other developers an website for the creation and life cycle management of RFCs.',
+        },
+        {
+          id: 2,
+          title: 'React Native app for building spections',
+          description:
+            'In my last project, I have built an app for a public organization focused on the inspection of buildings.',
+        },
+      ],
     },
     {
       id: 1,
       title: '#2 Company',
       date: 'Nov 2021 - Present',
-      role: 'Junior Developer in Front-End Development',
+      role: 'Mid Developer in Front-End Development',
       description:
         'Worked on creating websites, scripts and plugins for E-Commerces and Educational platforms using React and meta-frameworks such as NextJS and OSF.',
+      projects: [
+        {
+          id: 0,
+          title: 'OSF website',
+          description:
+            'In this project, I have create an OSF website for a famous company with the help of a wonderful team!',
+        },
+      ],
     },
   ]
+
+  function showCollapsedContent(experienceId: number) {
+    const element = window.document.querySelector(`#projects-${experienceId}`)!
+    element.classList.toggle('hidden')
+  }
+
+  function renderProjects(experienceId: number, projects: Project[]) {
+    return (
+      <ul id={`projects-${experienceId}`} className="hidden">
+        {projects.map((project, index) => {
+          return (
+            <li key={project.id} className="mb-4">
+              <h2 className="font-medium">
+                Project #{`${index + 1}`}: {project.title}
+              </h2>
+              <p>{project.description}</p>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  function renderExperiences(experiences: Experience[]) {
+    return experiences.map((experience) => {
+      return (
+        <div key={experience.id} className="flex items-center">
+          <div className="-ml-[calc(0.8rem)]">
+            <svg height="24" width="24">
+              <circle cx="12" cy="12" r="10" fill="white" />
+            </svg>
+          </div>
+
+          <div className="m-4 flex flex-col rounded-md bg-white p-4 text-black">
+            <h1 className="font-mono text-2xl">{experience.title}</h1>
+            <span className="font-semibold">
+              {experience.role} • {experience.date}
+            </span>
+            <span>{experience.description}</span>
+
+            <div className="mt-4">
+              <button
+                className="text-md bg-gray-800 font-mono font-medium text-white"
+                onClick={() => showCollapsedContent(experience.id)}
+              >
+                [View Projects]
+              </button>
+              {renderProjects(experience.id, experience.projects)}
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
 
   return (
     <section>
@@ -35,24 +120,7 @@ export function Timeline() {
         </div>
       </header>
       <div className="flex flex-col border-l-2 lg:m-auto lg:max-w-[calc(100vw-90vh)]">
-        {experiences.map((experience) => {
-          return (
-            <div key={experience.id} className="flex items-center">
-              <div className="-ml-[calc(0.8rem)]">
-                <svg height="24" width="24">
-                  <circle cx="12" cy="12" r="10" fill="white" />
-                </svg>
-              </div>
-              <div className="m-4 flex flex-col rounded-md bg-white p-4  text-black">
-                <h1 className="font-mono text-2xl">{experience.title}</h1>
-                <span className="font-semibold">
-                  {experience.role} • {experience.date}
-                </span>
-                <span>{experience.description}</span>
-              </div>
-            </div>
-          )
-        })}
+        {renderExperiences(experiences)}
       </div>
     </section>
   )
