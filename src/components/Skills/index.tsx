ab4d9584366487331f1dd8aeeb39245743c0a65b
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2023-05-31 06:55:30 -0300

    📱 fixing layout for mobile devices

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -11,13 +11,13 @@ export function Skills() {
         <h1 className="font-mono text-3xl font-bold">My skills</h1>
         <p className="mt-4 text-justify">{skills.text}</p>
       </header>
-      <div className="mt-4 grid grid-cols-2 content-center items-center justify-evenly justify-items-center">
+      <div className="mt-4 grid content-center items-center justify-evenly justify-items-center lg:grid-cols-2">
         <Techs techs={skills.techs} />
-        <div className="animate-smoothBounce rounded-full bg-white drop-shadow-md">
+        <div className="mt-12 animate-smoothBounce rounded-full bg-white drop-shadow-md lg:mt-0">
           <Image
             src={coding}
             alt="Human with a notebook"
-            className="lg:h-[24rem] lg:w-[24rem] h-80 w-80"
+            className="lg:h-[24rem] lg:w-[24rem] h-52 w-52"
             height={300}
             width={300}
           />
